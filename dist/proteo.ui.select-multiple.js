d7a6cdb0a6c9a843ac36c19d9bba8f3a5e2abf17
Roberto Zazo Lopez <roberto.zazo.lopez@everis.com>
2015-11-05 16:29:55 +0100

    update names of controller

--- dist/proteo.ui.select-multiple.js
+++ dist/proteo.ui.select-multiple.js
@@ -7,10 +7,12 @@ angular.module("proteo.ui.select-multiple", []).controller("selectMultipleCtrl",
 .directive("selectMultiple", function() {
 
 	console.log("Directive select-multiple");
+	
 	return {
 		restrict: "AE",
-		controller : 'selectMultipletCtrl',
+		controller : 'selectMultipleCtrl',
 		templateUrl: './bower_components/proteo.ui.buscador-contacto/dist/proteo.ui.select-multiple.tpl.html'
 	}
-});
+})
+;
 /* Todo Filter */
